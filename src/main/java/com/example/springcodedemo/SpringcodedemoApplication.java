43ee67812bbe2cc56b54490278b9aeb5cf194d04
sky-r1 <tanaso1412@gmail.com>
2023-07-03 00:43:57 +0900

    Setter Injection

--- src/main/java/com/example/springcodedemo/SpringcodedemoApplication.java
+++ src/main/java/com/example/springcodedemo/SpringcodedemoApplication.java
@@ -3,12 +3,6 @@ package com.example.springcodedemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-@SpringBootApplication(
-        scanBasePackages = {"com.example.springcodedemo",
-                "com.example.util"}
-)
-*/
 @SpringBootApplication
 public class SpringcodedemoApplication {
 
