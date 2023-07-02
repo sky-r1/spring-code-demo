55fdbfb2d58dc17f07f1f0f928543d7d897dc69e
sky-r1 <tanaso1412@gmail.com>
2023-07-02 23:46:00 +0900

    first commit

---
+++ src/main/java/com/example/springcodedemo/SpringcodedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcodedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcodedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcodedemo/SpringcodedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcodedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
